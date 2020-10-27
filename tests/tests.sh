9a4f891a812d7f860e729523e9117b4d59d085b6
Anthony K GROSS <anthony.k.gross@gmail.com>
2020-10-27 18:39:43 +0100

    Update tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 set -e
 
-RETURN=`docker run -e VAULT_KEY=1234 ansible-vault-cli-github-action:latest \
+ENCRYPTED_DATA=`docker run -e VAULT_KEY=1234 ansible-vault-cli-github-action:latest \
   "echo 'fake confidential data' > foo.yml \
   && ansible-vault encrypt foo.yml \
   && cat foo.yml"
 `
 SUB="ANSIBLE_VAULT;1.1;AES256"
 
-if [[ $RETURN != *"$SUB"* ]]; then
-  echo "File not encrypted."
+if [[ $ENCRYPTED_DATA != *"$SUB"* ]]; then
+  echo "Data not encrypted."
   exit 1
 fi
 
-if [[ $RETURN == "fake confidential data" ]]; then
-  echo "File not encrypted."
+if [[ $ENCRYPTED_DATA == "fake confidential data" ]]; then
+  echo "Data not encrypted."
   exit 1
 fi
+echo "Data encrypted."
 
-echo "File encrypted."
+ENCRYPTED_DATA=$(echo "$ENCRYPTED_DATA" | sed -r 's/[\$]+/\\\$/g')
+echo "$ENCRYPTED_DATA"
+
+DECRYPTED_DATA=`docker run -e VAULT_KEY=1234 ansible-vault-cli-github-action:latest \
+  "echo \"$ENCRYPTED_DATA\" > foo.yml \
+  && ansible-vault decrypt foo.yml \
+  && cat foo.yml"
+`
+
+
 exit 0
