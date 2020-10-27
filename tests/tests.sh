6e6f73e14fd78d5e725b5490b0fab29078389c59
Anthony K GROSS <anthony.k.gross@gmail.com>
2020-10-27 19:57:08 +0100

    end tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
+DATA='fake confidential data'
 ENCRYPTED_DATA=`docker run -e VAULT_KEY=1234 ansible-vault-cli-github-action:latest \
-  "echo 'fake confidential data' > foo.yml \
+  "echo '$DATA' > foo.yml \
   && ansible-vault encrypt foo.yml \
   && cat foo.yml"
 `
@@ -13,20 +14,24 @@ if [[ $ENCRYPTED_DATA != *"$SUB"* ]]; then
   exit 1
 fi
 
-if [[ $ENCRYPTED_DATA == "fake confidential data" ]]; then
+if [[ $ENCRYPTED_DATA == $DATA ]]; then
   echo "Data not encrypted."
   exit 1
 fi
 echo "Data encrypted."
 
-ENCRYPTED_DATA=$(echo "$ENCRYPTED_DATA" | sed -r 's/[\$]+/\\\$/g')
-echo "$ENCRYPTED_DATA"
+ENCRYPTED_DATA=$(echo "$ENCRYPTED_DATA" | sed -r 's/[\$]+/\\\$/g' | sed -r ':a;N;$!ba;s/\n/\\n/g')
 
 DECRYPTED_DATA=`docker run -e VAULT_KEY=1234 ansible-vault-cli-github-action:latest \
-  "echo \"$ENCRYPTED_DATA\" > foo.yml \
+  "echo -e \"$ENCRYPTED_DATA\" > foo.yml \
   && ansible-vault decrypt foo.yml \
   && cat foo.yml"
 `
 
+if [[ $DECRYPTED_DATA != $DATA ]]; then
+  echo "Data not decrypted."
+  exit 1
+fi
+echo "Data decrypted."
 
 exit 0
